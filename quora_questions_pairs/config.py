2df19fd41f72dd3c169607248976ef42f16cbf6b
abreukuse <t.kuse6@gmail.com>
2021-03-27 18:57:54 -0300

    Data is splited in the config.py file instead of training.py

--- quora_questions_pairs/config.py
+++ quora_questions_pairs/config.py
@@ -1,12 +1,11 @@
 import pathlib
 import pandas as pd
+from sklearn.model_selection import KFold
 
-PACKAGE_ROOT = pathlib.Path(quora_questions_pairs.__file__).resolve().parent
+PACKAGE_ROOT = pathlib.Path(__file__).resolve().parent
 DATASETS_DIR = PACKAGE_ROOT / 'datasets'
 TRAINED_MODEL_DIR = PACKAGE_ROOT / 'trained_models'
-
-X = pd.read_csv(DATASETS_DIR / 'train.csv', usecols=['question1', 'question2']).dropna()
-TARGET = pd.read_csv(DATASETS_DIR / 'train.csv', usecols=['is_duplicate'], squeeze=True).iloc[X.index]
+TRAINING_DATA_DIR = PACKAGE_ROOT / 'training_data'
 
 VARIABLES = ['question1', 'question2']
 RANDOM_STATE = 60
@@ -17,3 +16,22 @@ HYPERPARAMETERS = {'C': 1,
                    'max_iter': 1000,
                    'random_state': RANDOM_STATE,
                    'n_jobs': -1}
+
+X = pd.read_csv(DATASETS_DIR / 'train.csv', usecols=['question1', 'question2']).dropna()
+TARGET = pd.read_csv(DATASETS_DIR / 'train.csv', usecols=['is_duplicate'], squeeze=True).iloc[X.index]
+
+def data_split(X, y):
+    """Get the last fold in the cross validation process due to reproducibility purposes"""
+    folds = KFold(n_splits=SPLITS, shuffle=True, random_state=RANDOM_STATE)
+    train_indices, validation_indices = list(folds.split(X))[-1][0], list(folds.split(X))[-1][1]
+
+    X_train = X.iloc[train_indices]
+    X_validation = X.iloc[validation_indices]
+
+    y_train = y.iloc[train_indices]
+    y_validation = y.iloc[validation_indices]
+
+    return X_train, X_validation, y_train, y_validation
+
+X_TRAIN, X_VALIDATION, Y_TRAIN, Y_VALIDATION = data_split(X, TARGET)
+
