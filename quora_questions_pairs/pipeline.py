50bc2c550debd4ca7929695d4b73e07dd1cf65c9
abreukuse <t.kuse6@gmail.com>
2021-03-27 18:52:18 -0300

    imported StandarScaler and feature engine.

--- quora_questions_pairs/pipeline.py
+++ quora_questions_pairs/pipeline.py
@@ -1,28 +1,39 @@
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import FunctionTransformer
-from functions import basic_feature_engineering, fuzzy_features, drop_original_columns
-from config import VARIABLES, HYPERPARAMETERS
+from sklearn.preprocessing import FunctionTransformer, StandardScaler
+from sklearn.linear_model import LogisticRegression
+from feature_engine.wrappers import SklearnTransformerWrapper
+from quora_questions_pairs.config import VARIABLES, HYPERPARAMETERS
+from quora_questions_pairs.functions import (basic_feature_engineering, 
+										     fuzzy_features, 
+										     drop_original_columns,
+										     training_data_snapshot)
 
-pipeline = Pipeline([
-                     (
-                         'basic_feature_engineering', 
-                          FunctionTransformer(basic_feature_engineering)
-                      ),
-                     (
-                         'fuzzy_features', 
-                          FunctionTransformer(fuzzy_features)
-                      ),
-                     (
-                         'drop_original_columns',
-                         FunctionTransformer(drop_original_columns, kw_args={'variables': VARIABLES})
-                     ),
-                     (
-                         'Scaling',
-                         StandardScaler()
-                     ),
-                     (
-                         'algorithm',
-                         LogisticRegression(**HYPERPARAMETERS)
-                     )
-                     ]
-                    )
+
+training_pipeline = Pipeline([
+							 (
+								 'basic_feature_engineering', 
+								  FunctionTransformer(basic_feature_engineering)
+							  ),
+							 (
+								 'fuzzy_features', 
+								  FunctionTransformer(fuzzy_features)
+							  ),
+							 (
+								 'drop_original_columns',
+								 FunctionTransformer(drop_original_columns, 
+													 kw_args={'variables': VARIABLES})
+							 ),
+							 (
+								 'scaling',
+								 SklearnTransformerWrapper(transformer = StandardScaler())
+							 ),
+							 (
+								 'save_data_snapshot',
+								 FunctionTransformer(training_data_snapshot)
+							 ),
+							 (
+								 'algorithm',
+								 LogisticRegression(**HYPERPARAMETERS)
+							 )
+							 ]
+							)
