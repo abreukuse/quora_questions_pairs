9c57f9245e1040b87b0fe24591dab0435a83c3ff
abreukuse <49459900+abreukuse@users.noreply.github.com>
2021-03-28 15:46:31 -0300

    Fix indentation.

--- quora_questions_pairs/functions.py
+++ quora_questions_pairs/functions.py
@@ -15,19 +15,19 @@ def basic_feature_engineering(X):
 		X[f'len_word_q{i+1}'] = X[f'question{i+1}'].apply(lambda a: len(a.split()))
 	X['diff_len'] = X['len_q1'] - X['len_q2']
 	X['common_words'] = [len(set(a.lower().split()).intersection(set(b.lower().split()))) 
-						for a, b in zip(X['question1'], X['question2'])]
+			     for a, b in zip(X['question1'], X['question2'])]
 	return X
 
 def fuzzy_features(X):
 	"""Generate fuzzy based features"""
 	print('*Creating fuzzy features.')
 	attributes = ['QRatio',
-				  'WRatio',
-				  'partial_ratio',
-				  'partial_token_set_ratio',
-				  'partial_token_sort_ratio',
-				  'token_set_ratio',
-				  'token_sort_ratio']
+		      'WRatio',
+		      'partial_ratio',
+		      'partial_token_set_ratio',
+		      'partial_token_sort_ratio',
+		      'token_set_ratio',
+		      'token_sort_ratio']
 	for attribute in attributes:
 		print(f'-{attribute}')
 		X[f'fuzz_{attribute}'] = [getattr(fuzz, attribute)(a, b) for a, b in zip(X['question1'], X['question2'])]
@@ -42,7 +42,7 @@ def drop_original_columns(X, variables):
 def training_data_snapshot(X):
 	"""Save a sample of the training data right before it enters in the algorithm"""
 	if not os.path.isdir(TRAINING_DATA_DIR):
-			os.mkdir(TRAINING_DATA_DIR)
+		os.mkdir(TRAINING_DATA_DIR)
 		
 	file_path = TRAINING_DATA_DIR / f'training_data_v{version}.csv'
 
