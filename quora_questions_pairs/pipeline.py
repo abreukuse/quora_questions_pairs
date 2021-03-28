60f19cb3e8a36743cf8274031ecc7457b49f564e
abreukuse <49459900+abreukuse@users.noreply.github.com>
2021-03-28 15:42:41 -0300

    Adjusted indentation

--- quora_questions_pairs/pipeline.py
+++ quora_questions_pairs/pipeline.py
@@ -4,36 +4,36 @@ from sklearn.linear_model import LogisticRegression
 from feature_engine.wrappers import SklearnTransformerWrapper
 from quora_questions_pairs.config import VARIABLES, HYPERPARAMETERS
 from quora_questions_pairs.functions import (basic_feature_engineering, 
-										     fuzzy_features, 
-										     drop_original_columns,
-										     training_data_snapshot)
+					     fuzzy_features, 
+					     drop_original_columns,
+					     training_data_snapshot)
 
 
 training_pipeline = Pipeline([
-							 (
-								 'basic_feature_engineering', 
-								  FunctionTransformer(basic_feature_engineering)
-							  ),
-							 (
-								 'fuzzy_features', 
-								  FunctionTransformer(fuzzy_features)
-							  ),
-							 (
-								 'drop_original_columns',
-								 FunctionTransformer(drop_original_columns, 
-													 kw_args={'variables': VARIABLES})
-							 ),
-							 (
-								 'scaling',
-								 SklearnTransformerWrapper(transformer = StandardScaler())
-							 ),
-							 (
-								 'save_data_snapshot',
-								 FunctionTransformer(training_data_snapshot)
-							 ),
-							 (
-								 'algorithm',
-								 LogisticRegression(**HYPERPARAMETERS)
-							 )
-							 ]
-							)
+				 (
+					 'basic_feature_engineering', 
+					  FunctionTransformer(basic_feature_engineering)
+				  ),
+				 (
+					 'fuzzy_features', 
+					  FunctionTransformer(fuzzy_features)
+				  ),
+				 (
+					 'drop_original_columns',
+					 FunctionTransformer(drop_original_columns, 
+										 kw_args={'variables': VARIABLES})
+				 ),
+				 (
+					 'scaling',
+					 SklearnTransformerWrapper(transformer = StandardScaler())
+				 ),
+				 (
+					 'save_data_snapshot',
+					 FunctionTransformer(training_data_snapshot)
+				 ),
+				 (
+					 'algorithm',
+					 LogisticRegression(**HYPERPARAMETERS)
+				 )
+			     ]
+			   )
