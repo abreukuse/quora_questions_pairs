6372e1e5de2e1ee6e9819dc7227f7d2a815beedb
abreukuse <t.kuse6@gmail.com>
2021-04-04 17:49:25 -0300

    Deleted data_spit function from config file.

--- quora_questions_pairs/config.py
+++ quora_questions_pairs/config.py
@@ -19,19 +19,3 @@ HYPERPARAMETERS = {'C': 1,
 
 X = pd.read_csv(DATASETS_DIR / 'train.csv', usecols=['question1', 'question2']).dropna()
 TARGET = pd.read_csv(DATASETS_DIR / 'train.csv', usecols=['is_duplicate'], squeeze=True).iloc[X.index]
-
-def data_split(X, y):
-    """Get the last fold in the cross validation process due to reproducibility purposes"""
-    folds = KFold(n_splits=SPLITS, shuffle=True, random_state=RANDOM_STATE)
-    train_indices, validation_indices = list(folds.split(X))[-1][0], list(folds.split(X))[-1][1]
-
-    X_train = X.iloc[train_indices]
-    X_validation = X.iloc[validation_indices]
-
-    y_train = y.iloc[train_indices]
-    y_validation = y.iloc[validation_indices]
-
-    return X_train, X_validation, y_train, y_validation
-
-X_TRAIN, X_VALIDATION, Y_TRAIN, Y_VALIDATION = data_split(X, TARGET)
-
