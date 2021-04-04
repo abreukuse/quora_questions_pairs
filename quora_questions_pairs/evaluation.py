1155dd9bc922b60503edc9691fbea6911ac20026
abreukuse <t.kuse6@gmail.com>
2021-04-04 18:09:18 -0300

    Added evaluation.py file. It returns validation metrics.

---
+++ quora_questions_pairs/evaluation.py
@@ -0,0 +1,9 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+def evaluation(y_true, y_pred):
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+
+    return accuracy, precision, recall, f1
