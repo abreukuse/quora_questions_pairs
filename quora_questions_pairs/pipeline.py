28e70466b74df14e3d56fc21592e733b42ea09e8
abreukuse <t.kuse6@gmail.com>
2021-04-04 17:55:00 -0300

    Indentation.

--- quora_questions_pairs/pipeline.py
+++ quora_questions_pairs/pipeline.py
@@ -3,10 +3,7 @@ from sklearn.preprocessing import FunctionTransformer, StandardScaler
 from sklearn.linear_model import LogisticRegression
 from feature_engine.wrappers import SklearnTransformerWrapper
 from quora_questions_pairs.config import VARIABLES, HYPERPARAMETERS
-from quora_questions_pairs.functions import (basic_feature_engineering, 
-					     fuzzy_features, 
-					     drop_original_columns,
-					     training_data_snapshot)
+from quora_questions_pairs.functions import basic_feature_engineering, fuzzy_features, drop_original_columns, training_data_snapshot
 
 
 training_pipeline = Pipeline([
