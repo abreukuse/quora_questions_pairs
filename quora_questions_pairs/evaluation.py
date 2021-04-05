f85eea27a71f305e3f914c45c0ad47e087ef9981
abreukuse <t.kuse6@gmail.com>
2021-04-04 22:09:34 -0300

    The printing is done here from now on.

--- quora_questions_pairs/evaluation.py
+++ quora_questions_pairs/evaluation.py
@@ -1,9 +1,14 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 def evaluation(y_true, y_pred):
+    """Validation metrics"""
     accuracy = accuracy_score(y_true, y_pred)
     precision = precision_score(y_true, y_pred)
     recall = recall_score(y_true, y_pred)
     f1 = f1_score(y_true, y_pred)
 
-    return accuracy, precision, recall, f1
+    print(f'\n*** Validation Scores ***\n'
+          f'accuracy: {accuracy}\n'
+          f'precision: {precision}\n'
+          f'recall: {recall}\n'
+          f'f1_score: {f1}\n\n')
